53e2d6e3934565429cf6251b8600843b4ab7dc0a
Tosha Kamath <toshakamath@gmail.com>
2019-10-24 00:24:30 -0700

    set add new post fragment

--- app/src/main/java/com/example/wanderlust/ui/addnewpost/AddNewPostFragment.java
+++ app/src/main/java/com/example/wanderlust/ui/addnewpost/AddNewPostFragment.java
@@ -1,4 +1,4 @@
-package com.example.wanderlust.ui.notifications;
+package com.example.wanderlust.ui.addnewpost;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.wanderlust.R;
 
-public class NotificationsFragment extends Fragment {
+public class AddNewPostFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private AddNewPostViewModel addNewPostViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-        final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(this, new Observer<String>() {
+        addNewPostViewModel =
+                ViewModelProviders.of(this).get(AddNewPostViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_addnewpost, container, false);
+        final TextView textView = root.findViewById(R.id.text_addnewpost);
+        addNewPostViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
--- app/src/main/java/com/example/wanderlust/ui/addnewpost/AddNewPostViewModel.java
+++ app/src/main/java/com/example/wanderlust/ui/addnewpost/AddNewPostViewModel.java
@@ -1,16 +1,16 @@
-package com.example.wanderlust.ui.notifications;
+package com.example.wanderlust.ui.addnewpost;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class AddNewPostViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public AddNewPostViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is add new post fragment");
     }
 
     public LiveData<String> getText() {
