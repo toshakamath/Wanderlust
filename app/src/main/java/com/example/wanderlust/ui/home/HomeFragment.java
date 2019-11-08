a6263f6343253669d1fca65753f5dac8dd7ddb77
Tosha Kamath <toshakamath@gmail.com>
2019-11-07 16:04:44 -0800

    home page

--- app/src/main/java/com/example/wanderlust/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/wanderlust/ui/home/HomeFragment.java
@@ -14,7 +14,13 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.wanderlust.Adapters.BlogAdapter;
+import com.example.wanderlust.DatabaseHelper.DbInstance;
+import com.example.wanderlust.Doa.BlogObject;
+import com.example.wanderlust.MainActivity;
 import com.example.wanderlust.R;
 import com.example.wanderlust.ui.auth.Login;
 
@@ -23,6 +29,7 @@ import java.util.ArrayList;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private RecyclerView recycleCards;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,6 +43,26 @@ public class HomeFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+
+        recycleCards = (RecyclerView) root.findViewById(R.id.recycle_cards);
+
+        final DbInstance dbHelper = new DbInstance(getActivity());
+        final ArrayList<BlogObject> blogData = dbHelper.getAllBlogData();
+        BlogAdapter blogAdapter = new BlogAdapter(blogData);
+
+        for(int i=0; i<blogData.size(); i++){
+            Log.i("Tag: "+i, blogData.get(i).getBlogId());
+            Log.i("Tag: "+i, blogData.get(i).getBlogTitle());
+            Log.i("Tag: "+i, blogData.get(i).getBlogText());
+            Log.i("Tag: "+i, blogData.get(i).getBlogLocation());
+            Log.i("Tag: "+i, blogData.get(i).getBlogReviews());
+        }
+
+        recycleCards.setAdapter(blogAdapter);
+        recycleCards.setLayoutManager(new LinearLayoutManager(getContext()));
+
+//        blogData.clear();
+//        blogData.addAll(dbHelper.getAllBlogData());
 
         return root;
 
